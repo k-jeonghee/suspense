2599c0f75ffcee5a3e97fa0f6b3b07af6f340728
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-08-10 14:25:02 +0900

    chore: Rollup process 설정 추가

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import html from '@rollup/plugin-html';
 import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
 import typescript from '@rollup/plugin-typescript';
 import fs from 'fs';
 import path from 'path';
@@ -11,8 +12,10 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default {
-	input: 'src/index.ts',
+	input: 'src/index.tsx',
 	output: {
 		file: 'dist/bundle.js',
 		format: 'esm',
@@ -21,12 +24,22 @@ export default {
 		resolve(),
 		commonjs(),
 		typescript(),
+		replace({
+			preventAssignment: true,
+			'process.env.NODE_ENV': JSON.stringify(isProduction ? 'production' : 'development'),
+			process: JSON.stringify({
+				env: {
+					NODE_ENV: isProduction ? 'production' : 'development',
+				},
+			}),
+		}),
 		html({
 			template: () => {
 				const htmlTemplatePath = path.resolve(__dirname, './public/index.html');
 				return fs.readFileSync(htmlTemplatePath, 'utf8');
 			},
 		}),
+
 		serve({
 			open: true,
 			contentBase: ['dist'],
